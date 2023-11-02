7e8f672d131275dc26538ecc07b00c8cb6e92554
djsumin <judy3504@naver.com>
2023-11-02 14:41:47 +0900

    study vue3

---
+++ Vue3/Vue_day04_Single_File_Component/my-singer-project/src/components/SingerList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="singer-list-title">
+        <h2>가수 목록</h2>
+        <SingerDetail/>
+    </div>
+</template>
+
+<script setup>
+import SingerDetail from './singer/SingerDetail.vue';
+
+</script>
+
+<style scoped>
+.singer-list-title{
+    text-align: center;
+}
+</style>
---
+++ Vue3/Vue_day04_Single_File_Component/my-singer-project/src/components/singer/SingerDetail.vue
@@ -0,0 +1,21 @@
+<template>
+    
+    <div class="introduce">
+        <h2>아이유</h2>
+        <img src="../../assets/singer.jpg"  width="200"/>
+        <h4>이종석여자친구입니다.</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.introduce{
+    border: solid 1px plum;
+    margin: 20px;
+}
+
+
+</style>
